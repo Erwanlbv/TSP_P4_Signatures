c2bbaa059974381f7f04b92e10069d5c2c8f8424
Erwanlbv <er.leblevec@gmail.com>
2022-03-13 16:54:57 +0100

    Corrections d'erreurs sur les figures / Ajout de figures Partie II

--- clustering_methods.py
+++ clustering_methods.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from sklearn.cluster import KMeans
+from sklearn_extra.cluster import KMedoids
 
 
 def kmeans(dataset, n_clusters, random_state=None):
 
     km = KMeans(n_clusters=n_clusters, random_state=random_state).fit(dataset)
     return km
+
+
+def kmedoids(dataset, n_clusters, random_state=None):
+
+    kmd = KMedoids(n_clusters=n_clusters, random_state=random_state).fit(dataset)
+    return kmd
